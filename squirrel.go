d64ad4b5d9759a044ee4d4bbaa1caf2853059b95
Matthew Runo <matthew.s.runo@aexp.com>
2020-06-09 15:42:29 -0700

    Adding basic bugsnag sample code

--- squirrel.go
+++ squirrel.go
@@ -1,9 +1,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
+	"time"
 )
 
+// import "github.com/stianeikeland/go-rpio"
+import "github.com/bugsnag/bugsnag-go"
+
 func main() {
-	log.Print("Printing out something?")
+	bugsnag.Configure(bugsnag.Configuration{
+		APIKey: os.Getenv("BUGSNAG_KEY"),
+		// The import paths for the Go packages containing your source files
+		ProjectPackages: []string{"main", "github.com/inktomi/squirrel"},
+	})
+
+	err := bugsnag.Notify(fmt.Errorf("test error"))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	time.Sleep(10 * time.Second)
+
+	log.Print("Finished")
+
 }
