9b0b1be1ff97914cec77064522dfbeb7ad3c193b
Matthew Runo <74583+inktomi@users.noreply.github.com>
2020-06-10 16:44:03 -0700

    Testing pin output for buzzer

--- squirrel.go
+++ squirrel.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/stianeikeland/go-rpio"
 	"os"
 	"time"
-
-	"github.com/MichaelS11/go-hx711"
+	//"github.com/MichaelS11/go-hx711"
 )
 
-// import "github.com/stianeikeland/go-rpio"
 import "github.com/bugsnag/bugsnag-go"
 
 func main() {
@@ -18,37 +17,54 @@ func main() {
 		ProjectPackages: []string{"main", "github.com/inktomi/squirrel"},
 	})
 
-	err := hx711.HostInit()
+	err := rpio.Open()
 	if err != nil {
-		fmt.Println("HostInit error:", err)
+		fmt.Println("IO Error:", err)
 		return
 	}
 
-	hx711chip, err := hx711.NewHx711("GPIO5", "GPIO4")
-	if err != nil {
-		fmt.Println("NewHx711 error:", err)
-		return
-	}
+	pin := rpio.Pin(6)
 
-	defer hx711chip.Shutdown()
+	pin.Output()
 
-	err = hx711chip.Reset()
-	if err != nil {
-		fmt.Println("Reset error:", err)
-		return
-	}
-
-	var data int
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 1000; i++ {
+		time.Sleep(500 * time.Microsecond)
+		pin.High()
 		time.Sleep(200 * time.Microsecond)
-
-		data, err = hx711chip.ReadDataRaw()
-		if err != nil {
-			fmt.Println("ReadDataRaw error:", err)
-			continue
-		}
-
-		fmt.Println(data)
+		pin.Low()
 	}
+
+	//err := hx711.HostInit()
+	//if err != nil {
+	//	fmt.Println("HostInit error:", err)
+	//	return
+	//}
+	//
+	//hx711chip, err := hx711.NewHx711("GPIO5", "GPIO4")
+	//if err != nil {
+	//	fmt.Println("NewHx711 error:", err)
+	//	return
+	//}
+	//
+	//defer hx711chip.Shutdown()
+	//
+	//err = hx711chip.Reset()
+	//if err != nil {
+	//	fmt.Println("Reset error:", err)
+	//	return
+	//}
+	//
+	//var data int
+	//for i := 0; i < 10000; i++ {
+	//	time.Sleep(200 * time.Microsecond)
+	//
+	//	data, err = hx711chip.ReadDataRaw()
+	//	if err != nil {
+	//		fmt.Println("ReadDataRaw error:", err)
+	//		continue
+	//	}
+	//
+	//	fmt.Println(data)
+	//}
 
 }
