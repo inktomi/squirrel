b38bf895e13e86b4e0b84ec1a6e7f7555c479d96
Matthew Runo <74583+inktomi@users.noreply.github.com>
2020-06-25 18:11:40 -0700

    Fixed pin assignment

--- hardware/config.go
+++ hardware/config.go
@@ -1,7 +1,7 @@
 package hardware
 
-const Hx711Clk = "GPIO5"
-const Hx711Data = "GPIO17"
+const Hx711Clk = "GPIO17"
+const Hx711Data = "GPIO4"
 
 // The IO Pin used to control the buzzer
 const BUZZER = 6
